63f24370160395273149e3c2ea4f2c306f87d522
Yogesh <yogeshmittal120@gmail.com>
2019-10-23 17:09:10 +0530

    merge code

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import React,{Component} from "react";
 class Button extends Component{
     render(){
-        return <button>{this.props.name}</button> 
+        return <button>MY {this.props.name}</button> 
     }
 }
 export default Button;
